3dacbff3f0144bb353e7117b1564aef3cb9641e5
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-02-19 19:51:54 +0700

    fix: migration optimize table

--- src/migrations/1676810415295_optimize-tables.sql
+++ src/migrations/1676810415295_optimize-tables.sql
@@ -1,9 +1,4 @@
 -- Up Migration
-
-CREATE INDEX "attribute_keys_kind_index" ON "public"."attribute_keys" (
-  "kind" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" DESC NULLS LAST
-);
-
 ALTER TABLE "attribute_keys" SET (autovacuum_vacuum_scale_factor = 0.0);
 ALTER TABLE "attribute_keys" SET (autovacuum_vacuum_threshold = 5000);
 ALTER TABLE "attribute_keys" SET (autovacuum_analyze_scale_factor = 0.0);
@@ -21,8 +16,6 @@ ALTER TABLE "mq_jobs_data" SET (autovacuum_analyze_threshold = 5000);
 
 
 -- Down Migration
-DROP INDEX attribute_keys_kind_index;
-
 ALTER TABLE "attribute_keys" SET (autovacuum_vacuum_scale_factor = 0.05);
 ALTER TABLE "attribute_keys" SET (autovacuum_vacuum_threshold = 50);
 ALTER TABLE "attribute_keys" SET (autovacuum_analyze_scale_factor = NULL);
