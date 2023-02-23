22186efdab8a7bda42d2524a27d38d531bf6d9ce
Ryuzaki01 <shinji666mmc@gmail.com>
2023-02-23 14:41:29 +0700

    fix: on conflict source

--- packages/indexer/src/models/sources/index.ts
+++ packages/indexer/src/models/sources/index.ts
@@ -174,7 +174,7 @@ export class Sources {
           $/address/,
           $/metadata:json/
         )
-        ON CONFLICT (domain, domain_hash) DO UPDATE SET domain = EXCLUDED.domain
+        ON CONFLICT (domain) DO UPDATE SET domain = EXCLUDED.domain
         RETURNING *
       `,
       {
